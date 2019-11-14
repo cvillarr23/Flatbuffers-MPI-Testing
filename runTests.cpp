95a62fa03d60b16db78058d6b01e2c7508b3c846
cvillarr23 <cdvillarreal@wisc.edu>
2019-11-14 13:28:57 +0000

    Size test

--- runTests.cpp
+++ runTests.cpp
@@ -1,27 +1,25 @@
-#include <mpi.h>
-
+#include "messages_generated.h"
+#include <iostream>
 
 #define HEARTBEAT 5.0
 
 int main(int argc, char* argv[]) {
-  MPI_Init(argc, argv);
+  flatbuffers::FlatBufferBuilder _fbb;
 
-  int rank, numRanks;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &numRanks);
+  flatbuffers::Offset<AgentFlatbuf::Vector> vec = AgentFlatbuf::CreateVector(_fbb, 0, 1, 2);
+  // std::cout << vec.getSize() << std::endl;
 
-  double time;
-  double syncTime = 5.0;
-while(true) {
-  time = MPI_Wtime();
-  if(time > syncTime) {
-    syncTime += HEARTBEAT;
-    MPI_Alltoall(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, MPI_Comm comm)
-  }
+  flatbuffers::Offset<AgentFlatbuf::Quaternion> rot = AgentFlatbuf::CreateQuaternion(_fbb, 1.0, 2.0, 3.0, 4.0);
+  // std::cout << rot.getSize() << std::endl;
 
-}
+  flatbuffers::Offset<AgentFlatbuf::Orientation> ori = AgentFlatbuf::CreateOrientation(_fbb, vec, rot);
+  // std::cout << ori.getSize() << std::endl;
 
+  flatbuffers::Offset<AgentFlatbuf::State> state = AgentFlatbuf::CreateState(_fbb, 2.1, ori, ori, ori, ori, ori);
 
+  // AgentFlatbuf::StateBuilder builder_(_fbb);
+  // std::cout << builder_.getSize() << std::endl;
+  //size is 160
 
   return 0;
 }
